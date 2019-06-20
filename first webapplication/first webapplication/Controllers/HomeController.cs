d68a8598a408db8c453f039ce218942ad46a2768
Danish Ali <50976119+Danish-Ali097@users.noreply.github.com>
2019-06-20 20:11:54 +0500

    Initial commit

---
+++ first webapplication/first webapplication/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace first_webapplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string id)
+        {
+            return Content("Hello "+id+"!");
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
